41bb4449e11b515a64b1b301f1bc4c5fc57c04a9
Nguyễn Quang Thái <thai8763@gmail.com>
2017-04-15 17:53:57 +0700

    abc

--- public/angularJS/AccountController.js
+++ public/angularJS/AccountController.js
@@ -91,32 +91,32 @@ app.controller('AccountController', function($scope, $http, API) {
             numeral: true
         }
     };
-});
 
-$('#createAccount').on('hidden.bs.modal', function(){
-    $(this).find('form')[0].reset();
-});
+    $('#createAccount').on('hidden.bs.modal', function(){
+        $(this).find('form')[0].reset();
+    });
 
-$('#readAccount').on('show.bs.modal', function (event) {
-    var modal = $(this);
-    modal.find('.modal-title').text('Xem thông tin tài khoản');
-    modal.find('.modal-title').removeClass('w3-text-green');
-    modal.find('.modal-title').addClass('w3-text-blue');
-    modal.find('#name').attr('readOnly', true);
-    modal.find('#bank_account').attr('readOnly', true);
-    modal.find('#bank').attr('readOnly', true);
-    modal.find('#total').attr('readOnly', true);
-    modal.find('#submit').hide();
-    modal.find('#updateAccount').show();
-    modal.find('#updateAccount').click(function () {
-        modal.find('.modal-title').text('Sửa thông tin tài khoản');
-        modal.find('.modal-title').removeClass('w3-text-blue');
-        modal.find('.modal-title').addClass('w3-text-green');
-        modal.find('#name').removeAttr('readOnly');
-        modal.find('#bank_account').removeAttr('readOnly');
-        modal.find('#bank').removeAttr('readOnly');
-        modal.find('#total').removeAttr('readOnly');
-        modal.find('#updateAccount').hide();
-        modal.find('#submit').show();
+    $('#readAccount').on('show.bs.modal', function (event) {
+        var modal = $(this);
+        modal.find('.modal-title').text('Xem thông tin tài khoản');
+        modal.find('.modal-title').removeClass('w3-text-green');
+        modal.find('.modal-title').addClass('w3-text-blue');
+        modal.find('#name').attr('readOnly', true);
+        modal.find('#bank_account').attr('readOnly', true);
+        modal.find('#bank').attr('readOnly', true);
+        modal.find('#total').attr('readOnly', true);
+        modal.find('#submit').hide();
+        modal.find('#updateAccount').show();
+        modal.find('#updateAccount').click(function () {
+            modal.find('.modal-title').text('Sửa thông tin tài khoản');
+            modal.find('.modal-title').removeClass('w3-text-blue');
+            modal.find('.modal-title').addClass('w3-text-green');
+            modal.find('#name').removeAttr('readOnly');
+            modal.find('#bank_account').removeAttr('readOnly');
+            modal.find('#bank').removeAttr('readOnly');
+            modal.find('#total').removeAttr('readOnly');
+            modal.find('#updateAccount').hide();
+            modal.find('#submit').show();
+        });
     });
 });
