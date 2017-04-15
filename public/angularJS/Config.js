e72f041ae68f50d9c98b3e2c45adbe66dbbdb058
Nguyễn Quang Thái <thai8763@gmail.com>
2017-04-15 16:31:10 +0700

    acv

--- public/angularJS/Config.js
+++ public/angularJS/Config.js
@@ -10,4 +10,4 @@ $(document).ready(function () {
 });
 
 var app = angular.module('LaRose', ['angularUtils.directives.dirPagination', 'cleave.js', 'angucomplete-alt'])
-    .constant('API', '/larose/public/');
+    .constant('API', 'http://larose.herokuapp.com/');
