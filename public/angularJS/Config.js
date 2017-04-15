3ef7c9b88af85317cb3001ef7ee81bf290d4aa2f
Nguyễn Quang Thái <thai8763@gmail.com>
2017-04-15 19:54:57 +0700

    123

--- public/angularJS/Config.js
+++ public/angularJS/Config.js
@@ -11,21 +11,3 @@ $(document).ready(function () {
 
 var app = angular.module('LaRose', ['angularUtils.directives.dirPagination', 'cleave.js', 'angucomplete-alt'])
     .constant('API', 'http://larose-admin.herokuapp.com/');
-
-// Get the Sidebar
-var mySidebar = document.getElementById("mySidebar");
-
-
-// Toggle between showing and hiding the sidebar, and add overlay effect
-function w3_open() {
-    if (mySidebar.style.display === 'block') {
-        mySidebar.style.display = 'none';
-    } else {
-        mySidebar.style.display = 'block';
-    }
-}
-
-// Close the sidebar with the close button
-function w3_close() {
-    mySidebar.style.display = "none";
-}
--- public/js/validate.js
+++ public/js/validate.js
@@ -24,3 +24,21 @@ $('input[type="number"]')
     .on('input', function(){
         return this.setCustomValidity('');
     });
+
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+    } else {
+        mySidebar.style.display = 'block';
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
