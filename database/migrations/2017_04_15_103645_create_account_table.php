0d1f4729f0bc1714e616e0a6e580f290866adb52
Nguyễn Quang Thái <thai8763@gmail.com>
2017-04-15 17:40:04 +0700

    abc

---
+++ database/migrations/2017_04_15_103645_create_account_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	    Schema::create('account', function (Blueprint $table) {
+		    $table->increments('id');
+		    $table->string('name') -> nullable();
+		    $table->string('bank_account') -> unique();
+		    $table->string('bank') -> nullable();
+		    $table->integer('total') -> nullable();
+		    $table->timestamps();
+		    $table->softDeletes();
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    Schema::dropIfExists('account');
+    }
+}
