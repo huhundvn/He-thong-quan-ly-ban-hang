abf38d1866dca2db6f059e8703a9edcf9eb8b4eb
Nguyễn Quang Thái <thai8763@gmail.com>
2017-04-15 19:43:46 +0700

    Optimize

--- public/angularJS/Config.js
+++ public/angularJS/Config.js
@@ -9,5 +9,23 @@ $(document).ready(function () {
     });
 });
 
+// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+    } else {
+        mySidebar.style.display = 'block';
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
 var app = angular.module('LaRose', ['angularUtils.directives.dirPagination', 'cleave.js', 'angucomplete-alt'])
     .constant('API', 'http://larose-admin.herokuapp.com/');
