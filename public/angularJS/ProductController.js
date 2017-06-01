f87d611a62bc5c6c459bf68d8d1b97c22cfd9e65
Nguyễn Quang Thái <thai8763@gmail.com>
2017-06-01 10:10:34 +0700

    check 01

--- public/angularJS/ProductController.js
+++ public/angularJS/ProductController.js
@@ -3,21 +3,20 @@
  */
 
 app.controller('ProductController', function($scope, $http, API, $interval) {
+    $http.get(API + 'category').then(function (response) {
+        $scope.categorys = response.data;
+    }); // Load nhóm sản phẩm
+
+    $http.get(API + 'unit').then(function (response) {
+        $scope.units = response.data;
+    }); // Load đơn vị tính
+
+    $http.get(API + 'manufacturer').then(function (response) {
+        $scope.manufacturers = response.data;
+    }); // Load nhà sản xuất
 
     // Load danh sách sản phẩm
     $scope.loadProduct = function () {
-        $http.get(API + 'category').then(function (response) {
-            $scope.categorys = response.data;
-        }); // Load nhóm sản phẩm
-
-        $http.get(API + 'unit').then(function (response) {
-            $scope.units = response.data;
-        }); // Load đơn vị tính
-
-        $http.get(API + 'manufacturer').then(function (response) {
-            $scope.manufacturers = response.data;
-        }); // Load nhà sản xuất
-
         $http.get(API + 'attribute').then(function (response) {
             $scope.attributes = response.data;
         }); // Load thuộc tính sản phẩm
