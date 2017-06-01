f2587a0db1787fa83ac776160d588504b565346c
Nguyễn Quang Thái <thai8763@gmail.com>
2017-06-01 22:41:09 +0700

    123

--- public/angularJS/Config.js
+++ public/angularJS/Config.js
@@ -11,15 +11,15 @@ $(document).ready(function () {
 
 var app = angular.module('LaRose', ['angucomplete-alt', 'angularUtils.directives.dirPagination', 'cleave.js', 'chart.js'])
 	.constant('API', 'http://larose-admin.herokuapp.com/api/');
-    // .config(['ChartJsProvider', function (ChartJsProvider) {
-    //     // Configure all charts
-    //     ChartJsProvider.setOptions({
-    //         chartColors: ['#803690', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
-    //         responsive: true
-    //     });
-    //     // Configure all line charts
-    //     ChartJsProvider.setOptions('line', {
-    //         showLines: true
-    //     });
-    // }]);
+    .config(['ChartJsProvider', function (ChartJsProvider) {
+        // Configure all charts
+        ChartJsProvider.setOptions({
+            chartColors: ['#803690', '#00ADF9', '#DCDCDC', '#46BFBD', '#FDB45C', '#949FB1', '#4D5360'],
+            responsive: true
+        });
+        // Configure all line charts
+        ChartJsProvider.setOptions('line', {
+            showLines: true
+        });
+    }]);
 // http://larose-admin.herokuapp.com/api/
