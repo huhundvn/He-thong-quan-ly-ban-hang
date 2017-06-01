6b5f4dffabf720d519f749da53ea7629c5747b0f
Nguyễn Quang Thái <thai8763@gmail.com>
2017-06-01 12:33:16 +0700

    123

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -373,9 +373,9 @@
                                     <div class="form-group">
                                         <label class="col-sm-3"> Nhóm sản phẩm </label>
                                         <div class="col-sm-7">
-                                            <select id="category" ng-model="selected.category_id" class="form-control input-sm">
-                                                <option value="" selected> --Không chọn-- </option>
-                                                <option ng-repeat="category in categorys" ng-selected="selected.category_id===category.id" value="@{{category.id}}"> @{{category.name}} </option>
+                                            <select id="category" ng-options="category.name for category in categorys track by category.id" ng-model="selected.category_id" class="form-control input-sm">
+                                                <!-- <option value="" selected> --Không chọn-- </option>
+                                                <option ng-repeat="category in categorys" ng-selected="selected.category_id===category.id" value="@{{category.id}}"> @{{category.name}} </option> -->
                                             </select>
                                         </div>
                                         <div class="col-sm-2">
