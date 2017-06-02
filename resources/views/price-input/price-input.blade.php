0a1c3fad5dd6d664665e4a031e48203a1d2f78cd
Nguyễn Quang Thái <thai8763@gmail.com>
2017-06-02 11:52:01 +0700

    123

--- resources/views/price-input/price-input.blade.php
+++ resources/views/price-input/price-input.blade.php
@@ -15,12 +15,20 @@
         {{-- TÌM KIẾM BẢNG GIÁ --}}
         <div class="row">
             <div class="col-lg-2 col-xs-2">
+                <h3></h3>
                 <a href="{{route('createPriceInput')}}" class="btn btn-sm btn-success">
                     <span class="glyphicon glyphicon-plus"></span> Thêm mới </a>
             </div>
             <div class="col-lg-2 col-xs-2">
                 <label> Tên BG </label>
                 <input type="text" ng-model="term.name" class="form-control input-sm" placeholder="Nhập tên bảng giá...">
+            </div>
+            <div class="col-lg-2 col-xs-2">
+                <label> Nhà cung cấp </label>
+                <select ng-model="term2.supplier_id" class="form-control input-sm">
+                    <option value="" selected> -- Nhà cung cấp -- </option>
+                    <option value="@{{supplier.id}}"> @{{supplier.name}} </option>
+                </select>
             </div>
             <div class="col-lg-2 col-xs-2">
                 <label> Từ ngày </label>
@@ -32,7 +40,7 @@
             </div>
             <div class="col-lg-2 col-xs-2">
                 <label> Trạng thái </label>
-                <select ng-model="term2.status" class="form-control input-sm">
+                <select ng-model="term3.status" class="form-control input-sm">
                     <option value="" selected> -- Trạng thái -- </option>
                     <option value="1"> Chờ duyệt </option>
                     <option value="0"> Đã từ chối </option>
@@ -64,7 +72,7 @@
             </thead>
             <tbody>
             <tr ng-show="priceInputs.length > 0" class="item"
-                dir-paginate="priceInput in priceInputs | filter:term | filter:term2 | itemsPerPage: 7" ng-click="readPriceInput(priceInput)">
+                dir-paginate="priceInput in priceInputs | filter:term | filter:term2 | filter:term3 | itemsPerPage: 8" ng-click="readPriceInput(priceInput)">
                 <td data-toggle="modal" data-target="#readPriceOutput"> BGMH-@{{ priceInput.id }} </td>
                 <td data-toggle="modal" data-target="#readPriceOutput"> @{{ priceInput.name }} </td>
                 <td data-toggle="modal" data-target="#readPriceOutput" ng-repeat="supplier in suppliers" ng-show="supplier.id==priceInput.supplier_id">
