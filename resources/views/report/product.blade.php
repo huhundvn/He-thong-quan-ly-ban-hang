ed15b61ac544a4bc4f6774c581d21dadf0b53b9f
Nguyễn Quang Thái <thai8763@gmail.com>
2017-06-04 09:19:29 +0700

    123

--- resources/views/report/product.blade.php
+++ resources/views/report/product.blade.php
@@ -38,20 +38,11 @@
             </thead>
             <tbody>
                 <tr class="item" ng-if="topProducts.length > 0" dir-paginate="product in topProducts | itemsPerPage: 8" ng-click="readProduct(product)">
-                <td> SP@{{("000"+product.product_id).slice(-4)}} </td>
-                <td data-toggle="modal" data-target="#readProduct"> <p class="entry-text"> @{{ product.name}} </p> </td>
-                <td data-toggle="modal" data-target="#readProduct"> @{{ product.code }}</td>
-                <td data-toggle="modal" data-target="#readProduct"> @{{ product.unit.name }} </td>
-                <td data-toggle="modal" data-target="#readProduct"> @{{ product.total_quantity | number: 0 }}</td>
-                <td data-toggle="modal" data-target="#readProduct">
-                    <p ng-show="product.status == 1"> Còn hàng </p>
-                    <p ng-show="product.status == 0"> Hết hàng </p>
-                </td>
-                <td>
-                    <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteProduct" ng-show="0==product.status">
-                        <span class="glyphicon glyphicon-trash"></span>
-                    </button>
-                </td>
+                <td data-toggle="modal" data-target="#readProduct"> SP@{{("000"+product.product_id).slice(-4)}} </td>
+                <td data-toggle="modal" data-target="#readProduct" ng-repeat="item in products" ng-if="item.id==product.product_id"> @{{ item.name}} </td>
+                <td data-toggle="modal" data-target="#readProduct" ng-repeat="item in products" ng-if="item.id==product.product_id"> @{{ item.code }}</td>
+                <td data-toggle="modal" data-target="#readProduct" ng-repeat="item in products" ng-if="item.id==product.product_id"> @{{ item.unit.name }} </td>
+                <td data-toggle="modal" data-target="#readProduct"> @{{ product.sum | number: 0 }}</td>
             </tr>
             <tr class="item" ng-if="topProducts.length==0">
                 <td colspan="7"> Không có dữ liệu </td>
@@ -60,7 +51,7 @@
         </table>
 
         <div id="grid" class="container-fluid" hidden>
-            <!-- <h4 align="center"> Biểu đồ thống kê 10 sản phẩm bán chạy </h4> -->
+            <h4 align="center"> Biểu đồ thống kê 10 sản phẩm bán chạy </h4>
             <canvas class="chart-horizontal-bar" chart-series="series" chart-data="data" chart-labels="labels" ng-show="data.length>0"></canvas>
            	<h1 ng-show="data.length==0"> Không có dữ liệu </h1>
         </div>
