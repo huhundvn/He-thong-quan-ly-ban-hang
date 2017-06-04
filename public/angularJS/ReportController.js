068e61cca2575925ed8d99a0cc86bd31cf4a5f13
Nguyễn Quang Thái <thai8763@gmail.com>
2017-06-04 09:52:19 +0700

    123

--- public/angularJS/ReportController.js
+++ public/angularJS/ReportController.js
@@ -19,6 +19,18 @@ app.controller('ReportController', function($scope, $http, API, $interval) {
         $scope.products = response.data;
     });
 
+    $http.get(API + 'category').then(function (response) {
+        $scope.categorys = response.data;
+    }); // Load nhóm sản phẩm
+
+    $http.get(API + 'manufacturer').then(function (response) {
+        $scope.manufacturers = response.data;
+    }); // Load nhà sản xuất
+
+    $http.get(API + 'attribute').then(function (response) {
+        $scope.attributes = response.data;
+    }); // Load thuộc tính sản phẩm
+
     // LẤY DANH SÁCH 10 SẢN PHẨM BÁN CHẠY
     $http.get(API + 'top-product').then(function (response) {
         $scope.labels = [];
@@ -130,4 +142,57 @@ $("#viewGrid").click(function () {
     $("#viewGrid").addClass('w3-blue-grey');
     $("#list").attr('hidden', true);
     $("#grid").removeAttr('hidden');
+});
+
+$('#readProduct').on('show.bs.modal', function (event) {
+    var modal = $(this);
+    modal.find('.modal-title').text('Xem thông tin sản phẩm');
+    modal.find('.modal-title').removeClass('w3-text-green');
+    modal.find('.modal-title').addClass('w3-text-blue');
+    modal.find('#name').attr('readOnly', true);
+    modal.find('#code').attr('readOnly', true);
+    CKEDITOR.instances.description.setReadOnly(true);
+    CKEDITOR.instances.user_guide.setReadOnly(true);
+    var myDropzone = Dropzone.forElement(".dropzone");
+    myDropzone.removeAllFiles();
+    modal.find('#category').attr('disabled', true);
+    modal.find('#manufacturer').attr('disabled', true);
+    modal.find('#unit').attr('disabled', true);
+    modal.find('#web_price').attr('readOnly', true);
+    modal.find('#min_inventory').attr('readOnly', true);
+    modal.find('#max_inventory').attr('readOnly', true);
+    modal.find('#warranty_period').attr('readOnly', true);
+    modal.find('#return_period').attr('readOnly', true);
+    modal.find('#weight').attr('readOnly', true);
+    modal.find('#size').attr('readOnly', true);
+    modal.find('#volume').attr('readOnly', true);
+    modal.find('#addAttribute').attr('disabled', true);
+    modal.find('#addImage').attr('disabled', true);
+    modal.find('#submit').hide();
+    modal.find('#updateProduct').show();
+    modal.find('#updateProduct').click(function () {
+        modal.find('.modal-title').text('Sửa thông tin sản phẩm');
+        modal.find('.modal-title').removeClass('w3-text-blue');
+        modal.find('.modal-title').addClass('w3-text-green');
+        modal.find('#name').removeAttr('readOnly');
+        modal.find('#code').removeAttr('readOnly');
+        CKEDITOR.instances.description.setReadOnly(false);
+        CKEDITOR.instances.user_guide.setReadOnly(false);
+        modal.find('#user_guide').removeAttr('disabled');
+        modal.find('#category').removeAttr('disabled');
+        modal.find('#manufacturer').removeAttr('disabled');
+        modal.find('#unit').removeAttr('disabled');
+        modal.find('#web_price').removeAttr('readOnly');
+        modal.find('#min_inventory').removeAttr('readOnly');
+        modal.find('#max_inventory').removeAttr('readOnly');
+        modal.find('#warranty_period').removeAttr('readOnly');
+        modal.find('#return_period').removeAttr('readOnly');
+        modal.find('#weight').removeAttr('readOnly');
+        modal.find('#size').removeAttr('readOnly', true);
+        modal.find('#volume').removeAttr('readOnly', true);
+        modal.find('#addAttribute').removeAttr('disabled');
+        modal.find('#addImage').removeAttr('disabled');
+        modal.find('#updateProduct').hide();
+        modal.find('#submit').show();
+    });
 });
