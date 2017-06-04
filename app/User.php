301ba080a4407f1e3a6d53cc1777fec152551d94
Nguyễn Quang Thái <thai8763@gmail.com>
2017-06-04 10:32:55 +0700

    123

--- app/User.php
+++ app/User.php
@@ -49,6 +49,6 @@ class User extends Authenticatable
 
     public function store()
     {
-        return $this->belongsTo('App\Store');
+        return $this->belongsTo('App\Store', 'work_place_id', 'id');
     }
 }
