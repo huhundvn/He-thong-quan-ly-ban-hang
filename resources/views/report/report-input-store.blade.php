c4bd98513ed781ef62f281113dd5bfbfe719f10f
Nguyễn Quang Thái <thai8763@gmail.com>
2017-06-04 23:31:48 +0700

    123

--- resources/views/report/report-input-store.blade.php
+++ resources/views/report/report-input-store.blade.php
@@ -106,9 +106,9 @@
                                             <td> @{{item.created_at | date: "dd/MM/yyyy" }}</td>
                                             <td> YCNH-@{{item.id}} </td>
                                             <td>
-                                                <table style="width:100%; border: solid;">
+                                                <table style="width:100%;">
                                                     <tr ng-repeat="data in item.detail_input_stores">
-                                                        <td style="width:20%;" ng-repeat="product in products" ng-show="product.id==data.product_id"> @{{ product.name }} </td>
+                                                        <td style="width:20%; border: 1px" ng-repeat="product in products" ng-show="product.id==data.product_id"> @{{ product.name }} </td>
                                                         <td style="width:20%;" ng-repeat="product in products" ng-show="product.id==data.product_id"> @{{ product.unit.name }} </td>
                                                         <td style="width:20%;"> @{{ data.quantity | number: 0}} </td>
                                                         <td style="width:20%;"> @{{data.price_input | number:0 }} </td>
